015ec92b3d1d58fc3b4582497d77685ecffe7d33
mohsinkhanktk44 <64396821+mohsinkhanktk44@users.noreply.github.com>
2024-02-01 01:42:37 +0500

    feat:just sliderremainig

--- src/components/ZinCard.tsx
+++ src/components/ZinCard.tsx
@@ -3,21 +3,26 @@ type props = {
   title: string;
   description: string;
   img: string;
+  className?: string;
 };
-// this is just an for mergining purpose
-const ZinCard = (props: props) => {
-  const { title, description, img } = props;
-  return (
-    <div className="flex justify-center items-center">
-      <div className="max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img src={img} alt="" className="w-9 h-9" />
-        <h5 className="mb-2 text-xl font-semibold mt-2 tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h5>
 
-        <p className="mb-3 text-sm font-normal text-gray-500 dark:text-gray-400">
-          {description}
-        </p>
+const ZinCard = (props: props) => {
+  const { title, description, img, className } = props;
+  return (
+    <div className={`flex justify-center items-center hover:shadow-md `}>
+      <div
+        className={`${className} flex justify-between items-start flex-col py-6 px-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700`}
+      >
+        <img src={img} alt="" className="w-10 h-10" />
+        <div className="flex flex-col mt-3 ">
+          <h5 className="mb-2 text-[20px] font-bold  tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            {description}
+          </p>
+        </div>
       </div>
     </div>
   );
